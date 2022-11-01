6f82fcae818007df6c780fb0e6a9a15a6f6122e8
AliAl-Gburi <r0684462@ucll.be>
2022-11-01 10:08:47 +0100

    attempt at fixing backups

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,4 +6,3 @@ if echo $1 | grep -qP "\..*\.sw(p|x|px)$" ; then
 fi
 cp -p --parents $1 /var/backups/inotify
 mv /var/backups/inotify$1 /var/backups/inotify$1_$(date +'%Y.%m.%d_%H:%M:%S')
-scp -P 22345 /var/backups/inotify$1_$(date +'%Y.%m.%d_%H:%M:%S') r0684462@leia.uclllabs.be:/home/LDAP/r0684462/backups
---
+++ scripts/leiab.sh
@@ -0,0 +1,1 @@
+scp -r -P 22345 /etc r0684462@leia.uclllabs.be:/home/LDAP/r0684462/backups
